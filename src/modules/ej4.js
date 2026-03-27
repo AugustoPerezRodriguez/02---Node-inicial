86f4db3469de052d29faab8d2f5b0b607e87fc5e
AUGUSTO PÉREZ RODRIGUEZ <48794823@ort>
2026-03-27 08:36:03 -0300

    Ej4

--- src/modules/ej4.js
+++ src/modules/ej4.js
@@ -9,7 +9,25 @@ console.clear();
 
 copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
 
-function copiar(origen, destino){
-    const contenido = fs.readFileSync(origen, 'utf-8');
-    fs.writeFileSync(destino, contenido);
+async function copiar(origen, destino){
+
+    let archivoOrigen;
+    let archivoDestino;
+
+    try {
+        archivoOrigen = await fsPromises.open(origen, 'r'); 
+        archivoDestino = await fsPromises.open(destino, 'r'); 
+
+        const contenido = await archivoOrigen.read('utf-8');
+
+        await archivoDestino.writeFile(contenido);
+
+        console.log("Archivo copiado correctamente 🚀");
+
+    } catch (error) {
+        console.log("Error:", error);
+    } finally {
+        if (archivoOrigen) await archivoOrigen.close();
+        if (archivoDestino) await archivoDestino.close();
+    }
 }
